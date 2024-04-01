72ff0cba55bce0fdf94d1ec52077f38faf61f752
Ruben Movsisyan <rubenmovsisyan@Rubens-MacBook-Pro-2.local>
2024-04-01 13:13:26 -0700

    initial commit

---
+++ HPTrivia/HPTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPTriviaApp.swift
+//  HPTrivia
+//
+//  Created by Ruben Movsisyan on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct HPTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
