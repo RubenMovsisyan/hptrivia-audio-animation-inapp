9946e6b88fbaa61ba6061789f72fcc5b1e5b7f10
Ruben Movsisyan <rubenmovsisyan@Rubens-MacBook-Pro-2.local>
2024-04-02 23:27:47 -0700

    craeted celebration screen with some animations. Added couple of animations to gameplay screen

--- HPTrivia/Gameplay.swift
+++ HPTrivia/Gameplay.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct Gameplay: View {
     @State private var animateViewsIn = false
+    @State private var tappedCorrectAnswer = false
+    @State private var hintWiggle = false
+    @State private var scaleNextButtnon = false
+    @State private var movePointsToScore = false
     
     var body: some View {
         GeometryReader { geo in
@@ -55,10 +59,15 @@ struct Gameplay: View {
                                     .scaledToFit()
                                     .frame(width: 100)
                                     .foregroundStyle(.cyan)
-                                    .rotationEffect(.degrees(-15))
+                                    .rotationEffect(.degrees(hintWiggle ? -13 : -17))
                                     .padding()
                                     .padding(.leading, 20)
                                     .transition(.offset(x: -geo.size.width / 2))
+                                    .onAppear {
+                                        withAnimation(.easeInOut(duration: 0.1).repeatCount(9).delay(5).repeatForever()) {
+                                             hintWiggle = true
+                                        }
+                                    }
                             }
                         }
                         .animation(.easeOut(duration: 1.5).delay(2), value: animateViewsIn)
@@ -75,10 +84,15 @@ struct Gameplay: View {
                                     .frame(width: 100, height: 100)
                                     .background(.cyan)
                                     .clipShape(.rect(cornerRadius: 20))
-                                    .rotationEffect(.degrees(15))
+                                    .rotationEffect(.degrees(hintWiggle ? 13 : 17))
                                     .padding()
                                     .padding(.trailing, 20)
                                     .transition(.offset(x: geo.size.width / 2))
+                                    .onAppear {
+                                        withAnimation(.easeInOut(duration: 0.1).repeatCount(9).delay(5).repeatForever()) {
+                                             hintWiggle = true
+                                        }
+                                    }
                             }
                         }
                         .animation(.easeOut(duration: 1.5).delay(2), value: animateViewsIn)
@@ -109,12 +123,86 @@ struct Gameplay: View {
                 .frame(width: geo.size.width, height: geo.size.height)
                 .foregroundStyle(.white)
                     
+                VStack {
+                    Spacer()
+                    
+                    VStack {
+                        if tappedCorrectAnswer {
+                            Text("5")
+                                .font(.largeTitle)
+                                .padding(.top, 50)
+                                .transition(.offset(y: -geo.size.height / 4))
+                                .offset(x: movePointsToScore ? geo.size.width / 2.3 : 0,
+                                        y: movePointsToScore ? -geo.size.height / 13: 0)
+                                .opacity(movePointsToScore ? 0 : 1)
+                                .onAppear {
+                                    withAnimation(.easeInOut(duration: 1).delay(3)) {
+                                        movePointsToScore = true
+                                    }
+                                }
+                        }
+                    }
+                    .animation(.easeInOut(duration: 1).delay(2), value: tappedCorrectAnswer)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        if tappedCorrectAnswer {
+                            Text("Brilliant!")
+                                .font(.custom(Consts.hpFont, size: 100))
+                                .transition(.scale.combined(with: .offset(y: -geo.size.height / 2)))
+                        }
+                    }
+                    .animation(.easeInOut(duration: 1).delay(1), value: tappedCorrectAnswer)
+                    
+                    Spacer()
+                    
+                   
+                    if tappedCorrectAnswer {
+                        Text("Answer1")
+                            .minimumScaleFactor(0.5)
+                            .multilineTextAlignment(.center)
+                            .padding(10)
+                            .frame(width: geo.size.width / 2.15, height: 80)
+                            .background(.green.opacity(0.5))
+                            .clipShape(.rect(cornerRadius: 25))
+                            .scaleEffect(2)
+                    }
+                    
+                    Spacer()
+                    Spacer()
+                    
+                    VStack {
+                        if tappedCorrectAnswer {
+                            Button("Next Level>") {
+                                
+                            }
+                            .buttonStyle(.borderedProminent)
+                            .tint(.blue.opacity(0.5))
+                            .font(.largeTitle)
+                            .transition(.offset(y: geo.size.height / 3))
+                            .scaleEffect(scaleNextButtnon ? 1.2 : 1)
+                            .onAppear {
+                                withAnimation(.easeInOut(duration: 1.45).repeatForever()) {
+                                    scaleNextButtnon.toggle()
+                                }
+                            }
+                        }
+                    }
+                    .animation(.easeInOut(duration: 2.7).delay(2.7), value: tappedCorrectAnswer)
+
+                    Spacer()
+                    Spacer()
+                }
+                .foregroundStyle(.white)
+                
             }
             .frame(width: geo.size.width, height: geo.size.height)
         }
         .ignoresSafeArea()
         .onAppear {
-            animateViewsIn = true
+//            animateViewsIn = true
+            tappedCorrectAnswer = true
         }
     }
 }
